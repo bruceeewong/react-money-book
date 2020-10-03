b503879f8bb4025b6797d85da0cd13f2df9ad6d1
bruskiwang <bruskiwang@tencent.com>
2020-10-03 10:20:53 +0800

    feat(component): 新增PriceList组件

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PriceList from './components/PriceList';
+
+const items = [
+  {
+    "id": 1,
+    "title": "工资",
+    "price": 12000,
+    "date": "2018-09-10",
+    "category": {
+      "id": 1,
+      "name": "工资",
+      "type": "income"
+    }
+  },
+  {
+    "id": 2,
+    "title": "去云南旅游",
+    "price": 200,
+    "date": "2018-09-10",
+    "category": {
+      "id": 2,
+      "name": "旅行",
+      "type": "outcome"
+    }
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <PriceList 
+        items={items}
+        onModifyItem={(item) => alert(item.id)}
+        onDeleteItem={(item) => alert(item.id)}
+      ></PriceList>
     </div>
   );
 }
